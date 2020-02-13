55ea94a83f6d07457e0f44e59a1bbf86acf59ea0
xTCry (YSTU A-255_4) <vlad@xtashs.ru>
2020-02-13 18:01:04 +0300

    Init repos

---
+++ ru.xtashs.is.labs/src/ru/xtashs/is/labs/LabX.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ru.xtashs.is.labs;
+
+/**
+ * @author stud
+ *
+ */
+public class LabX {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("HelloW");
+	}
+
+}
