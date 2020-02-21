b54fffa4da511d026a71f3f43cab4e98081fb854
xTCry (YSTU A-255_4) <vlad@xtashs.ru>
2020-02-21 15:12:32 +0300

    Update (Lab1)

--- ru.xtashs.is.labs/src/ru/xtashs/is/labs/LabX.java
+++ ru.xtashs.is.labs/src/ru/xtashs/is/labs/LabX.java
@@ -3,6 +3,9 @@
  */
 package ru.xtashs.is.labs;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * @author stud
@@ -10,13 +13,23 @@ package ru.xtashs.is.labs;
  */
 public class LabX {
 
-	/**
-	 * @param args
-	 */
+	public static BufferedReader in;
+	
+	static {
+		in = new BufferedReader(new InputStreamReader(System.in));
+	}
+
 	public static void main(String[] args) {
-		Lab_1 lab = new Lab_1();
-		lab.main();
-		cls();
+		LabX lab = new LabX();
+
+		Lab_1 lab1 = new Lab_1();
+		lab1.main(lab);
+	}
+	
+	public final static String readLine(String p) throws IOException {
+		System.out.print(p);
+		System.out.flush();
+		return in.readLine();
 	}
 	
 	public final static void cls() {
--- ru.xtashs.is.labs/src/ru/xtashs/is/labs/Lab_1.java
+++ ru.xtashs.is.labs/src/ru/xtashs/is/labs/Lab_1.java
@@ -9,8 +9,66 @@ package ru.xtashs.is.labs;
  */
 public class Lab_1 {
 
-	public void main() {
+	public void main(LabX lab) {
 		System.out.println("Laba 1 out data;");
+		Ex1();
+		System.out.println("--------------\n");
+		Ex2();
 	}
 	
+	private void Ex1() {
+		// Variant 7
+		final double a = 4.21,
+			b = 0.89,
+			minX = -3,
+			maxX = 3,
+			delatX = 1;
+		
+		double x = minX;
+		System.out.println("Ex1\n==========");
+		System.out.println("x\t\ty\t\t");
+		
+		while (x <= maxX) {
+			double y = 0;
+			if (x <= b) {
+				y = Math.sqrt(b - x);
+			}
+			else if (b < x && x < a) {
+				y = (x + a) / (Math.exp(a) + Math.exp(x));
+			}
+			else if (x >= a) {
+				y = a * x + b;
+			}
+			
+			System.out.println(x + "\t\t" + y);
+			x += delatX;
+		}
+	}
+	
+	private void Ex2() {
+		// Variant X
+		final double minX = -0.1,
+			maxX = 0.9,
+			delatX = 0.1,
+			eps = 1e-5;
+		
+		double x = minX;
+		System.out.println("Ex2\n==========");
+		System.out.println("x\t\ty");
+
+		while (x <= maxX){
+			double T = 1,
+				y = 0;
+			int k = 0;
+			
+			while(Math.abs(T) > eps){
+				y += T;
+				T *= (x * k + x) / k;
+				k++;
+			}
+			
+			System.out.println(x + "\t\t" + y);
+			x += delatX;
+		}
+	}
 }
